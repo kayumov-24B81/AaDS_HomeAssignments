0ba70ed7101500641a0e7b106a93529c6e9afc7f
Airat Kayumov <st128100@student.spbu.ru>
2025-03-19 23:46:08 +0300

    Added simple test file and modified CI configuration file from teacher's repo.

---
+++ HomeAssignment1/ascii85-test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "encoder.hpp"
+
+TEST(Sampletest, Sampletest1)
+{
+    EXPECT_TRUE(true);
+}
+
+int main()
+{
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
